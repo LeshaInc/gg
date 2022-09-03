8bcacbcc4e29d79a7ee6819ccd32b3a07d7ea1c1
LeshaInc <includeurl@gmail.com>
2022-09-03 17:56:27 +0300

    Less copying data between registers

--- gg-expr/src/new_compiler/mod.rs
+++ gg-expr/src/new_compiler/mod.rs
@@ -58,7 +58,21 @@ impl Compiler {
         ))
     }
 
-    pub fn compile_expr(&mut self, expr: Expr, dst: RegId) {
+    fn compile_const(&mut self, value: impl Into<Value>, dst: RegId) {
+        let id = self.consts.add(value.into());
+        self.instrs.add(Instr::LoadConst { id, dst });
+    }
+
+    fn compile_expr_dst(&mut self, expr: Expr, dst: RegId) {
+        let mut tmp = dst;
+        self.compile_expr(expr, &mut tmp);
+
+        if tmp != dst {
+            self.instrs.add(Instr::Copy { src: tmp, dst });
+        }
+    }
+
+    fn compile_expr(&mut self, expr: Expr, dst: &mut RegId) {
         match expr {
             Expr::Null(expr) => self.compile_expr_null(expr, dst),
             Expr::Bool(expr) => self.compile_expr_bool(expr, dst),
@@ -80,36 +94,31 @@ impl Compiler {
         }
     }
 
-    fn compile_const(&mut self, value: impl Into<Value>, dst: RegId) {
-        let id = self.consts.add(value.into());
-        self.instrs.add(Instr::LoadConst { id, dst });
+    fn compile_expr_null(&mut self, _expr: ExprNull, dst: &mut RegId) {
+        self.compile_const(Value::null(), *dst)
     }
 
-    fn compile_expr_null(&mut self, _expr: ExprNull, dst: RegId) {
-        self.compile_const(Value::null(), dst)
-    }
-
-    fn compile_expr_bool(&mut self, expr: ExprBool, dst: RegId) {
+    fn compile_expr_bool(&mut self, expr: ExprBool, dst: &mut RegId) {
         let value = expr.value().unwrap_or_default();
-        self.compile_const(value, dst)
+        self.compile_const(value, *dst)
     }
 
-    fn compile_expr_int(&mut self, expr: ExprInt, dst: RegId) {
+    fn compile_expr_int(&mut self, expr: ExprInt, dst: &mut RegId) {
         let value = expr.value().unwrap_or_default();
-        self.compile_const(value, dst)
+        self.compile_const(value, *dst)
     }
 
-    fn compile_expr_float(&mut self, expr: ExprFloat, dst: RegId) {
+    fn compile_expr_float(&mut self, expr: ExprFloat, dst: &mut RegId) {
         let value = expr.value().unwrap_or_default();
-        self.compile_const(value, dst)
+        self.compile_const(value, *dst)
     }
 
-    fn compile_expr_string(&mut self, expr: ExprString, dst: RegId) {
+    fn compile_expr_string(&mut self, expr: ExprString, dst: &mut RegId) {
         let value = expr.value().unwrap_or_default();
-        self.compile_const(value, dst)
+        self.compile_const(value, *dst)
     }
 
-    fn compile_expr_binding(&mut self, expr: ExprBinding, dst: RegId) {
+    fn compile_expr_binding(&mut self, expr: ExprBinding, dst: &mut RegId) {
         let ident = match expr.ident() {
             Some(v) => v,
             None => return,
@@ -121,9 +130,7 @@ impl Compiler {
         };
 
         match loc {
-            VarLocation::Reg(src) => {
-                self.instrs.add(Instr::Copy { src, dst });
-            }
+            VarLocation::Reg(reg) => *dst = reg,
             VarLocation::Capture(_) => todo!(),
         }
     }
@@ -168,110 +175,123 @@ impl Compiler {
         self.add_error(diagnostic);
     }
 
-    fn compile_expr_binary(&mut self, expr: ExprBinary, dst: RegId) {
-        let lhs = self.regs.alloc();
+    fn compile_expr_binary(&mut self, expr: ExprBinary, dst: &mut RegId) {
+        let mut lhs = *dst;
+
         if let Some(expr) = expr.lhs() {
-            self.compile_expr(expr, lhs);
+            self.compile_expr(expr, &mut lhs);
         }
 
-        let rhs = dst;
+        let mut rhs = *dst;
+        let mut rhs_tmp = None;
+        if lhs == rhs {
+            rhs = self.regs.alloc();
+            rhs_tmp = Some(rhs);
+        }
+
+        let mut rhs = *dst;
         if let Some(expr) = expr.rhs() {
-            self.compile_expr(expr, rhs);
+            self.compile_expr(expr, &mut rhs);
         }
 
-        let op = expr.op().unwrap_or(BinOp::Add);
+        self.instrs.add(Instr::BinOp {
+            op: expr.op().unwrap_or(BinOp::Add),
+            lhs,
+            rhs,
+            dst: *dst,
+        });
 
-        self.regs.free(lhs);
-        self.instrs.add(Instr::BinOp { op, lhs, rhs, dst });
+        if let Some(tmp) = rhs_tmp {
+            self.regs.free(tmp);
+        }
     }
 
-    fn compile_expr_unary(&mut self, expr: ExprUnary, dst: RegId) {
+    fn compile_expr_unary(&mut self, expr: ExprUnary, dst: &mut RegId) {
+        let mut arg = *dst;
         if let Some(expr) = expr.expr() {
-            self.compile_expr(expr, dst);
+            self.compile_expr(expr, &mut arg);
         }
 
         let op = expr.op().unwrap_or(UnOp::Neg);
-        self.instrs.add(Instr::UnOp { op, arg: dst, dst });
+        self.instrs.add(Instr::UnOp { op, arg, dst: *dst });
     }
 
-    fn compile_expr_grouped(&mut self, expr: ExprGrouped, dst: RegId) {
+    fn compile_expr_grouped(&mut self, expr: ExprGrouped, dst: &mut RegId) {
         if let Some(expr) = expr.expr() {
             self.compile_expr(expr, dst)
         }
     }
 
-    fn compile_expr_list(&mut self, expr: ExprList, dst: RegId) {
+    fn compile_expr_list(&mut self, expr: ExprList, dst: &mut RegId) {
         let len = expr.exprs().count() as u16;
         let seq = self.regs.alloc_seq(len);
 
         for (expr, reg) in expr.exprs().zip(seq) {
-            self.compile_expr(expr, reg);
+            self.compile_expr_dst(expr, reg);
         }
 
-        self.instrs.add(Instr::NewList { seq, dst });
+        self.instrs.add(Instr::NewList { seq, dst: *dst });
         self.regs.free_seq(seq);
     }
 
-    fn compile_expr_map(&mut self, expr: ExprMap, dst: RegId) {
+    fn compile_expr_map(&mut self, expr: ExprMap, dst: &mut RegId) {
         let len = expr.pairs().count() as u16;
         let seq = self.regs.alloc_seq(len * 2);
 
         for (pair, reg) in expr.pairs().zip(seq.into_iter().step_by(2)) {
             if let Some(expr) = pair.key_expr() {
-                self.compile_expr(expr, reg);
-            }
-
-            if let Some(ident) = pair.key_ident() {
+                self.compile_expr_dst(expr, reg);
+            } else if let Some(ident) = pair.key_ident() {
                 self.compile_const(ident.name(), reg);
             }
 
             if let Some(expr) = pair.value() {
-                self.compile_expr(expr, RegId(reg.0 + 1));
+                self.compile_expr_dst(expr, RegId(reg.0 + 1));
             }
         }
 
-        self.instrs.add(Instr::NewMap { seq, dst });
+        self.instrs.add(Instr::NewMap { seq, dst: *dst });
         self.regs.free_seq(seq);
     }
 
-    fn compile_expr_call(&mut self, expr: ExprCall, dst: RegId) {
+    fn compile_expr_call(&mut self, expr: ExprCall, dst: &mut RegId) {
         let arity = expr.args().count() as u16;
         let seq = self.regs.alloc_seq(arity + 1);
 
         if let Some(expr) = expr.func() {
-            self.compile_expr(expr, seq.base);
+            self.compile_expr_dst(expr, seq.base);
         }
 
         for (expr, reg) in expr.args().zip(seq.into_iter().skip(1)) {
-            self.compile_expr(expr, reg);
+            self.compile_expr_dst(expr, reg);
         }
 
-        self.instrs.add(Instr::Call { seq, dst });
+        self.instrs.add(Instr::Call { seq, dst: *dst });
         self.regs.free_seq(seq);
     }
 
-    fn compile_expr_index(&mut self, _expr: ExprIndex, _dst: RegId) {
+    fn compile_expr_index(&mut self, _expr: ExprIndex, _dst: &mut RegId) {
         todo!()
     }
 
-    fn compile_expr_if_else(&mut self, expr: ExprIfElse, dst: RegId) {
+    fn compile_expr_if_else(&mut self, expr: ExprIfElse, dst: &mut RegId) {
         let cond = self.regs.alloc();
 
         if let Some(expr) = expr.cond() {
-            self.compile_expr(expr, cond);
+            self.compile_expr_dst(expr, cond);
         }
 
         let start = self.instrs.add(Instr::Nop);
         self.regs.free(cond);
 
         if let Some(expr) = expr.if_false() {
-            self.compile_expr(expr, dst);
+            self.compile_expr_dst(expr, *dst);
         }
 
         let mid = self.instrs.add(Instr::Nop);
 
         if let Some(expr) = expr.if_true() {
-            self.compile_expr(expr, dst);
+            self.compile_expr_dst(expr, *dst);
         }
 
         let end = self.instrs.next_idx();
@@ -283,14 +303,14 @@ impl Compiler {
         self.instrs.set(mid, Instr::Jump { offset })
     }
 
-    fn compile_expr_let_in(&mut self, expr: ExprLetIn, dst: RegId) {
+    fn compile_expr_let_in(&mut self, expr: ExprLetIn, dst: &mut RegId) {
         self.push_scope();
 
         for binding in expr.bindings() {
             let reg = self.regs.alloc();
 
             if let Some(expr) = binding.expr() {
-                self.compile_expr(expr, reg);
+                self.compile_expr_dst(expr, reg);
             }
 
             if let Some(ident) = binding.ident() {
@@ -305,12 +325,16 @@ impl Compiler {
         self.pop_scope();
     }
 
-    fn compile_expr_when(&mut self, expr: ExprWhen, dst: RegId) {
-        let value = self.regs.alloc();
+    fn compile_expr_when(&mut self, expr: ExprWhen, dst: &mut RegId) {
+        let value_tmp = self.regs.alloc();
+        let mut value = value_tmp;
         let cond = self.regs.alloc();
 
         if let Some(expr) = expr.expr() {
-            self.compile_expr(expr, value);
+            self.compile_expr(expr, &mut value);
+            if value != value_tmp {
+                self.regs.free(value);
+            }
         }
 
         let mut holes = Vec::new();
@@ -326,7 +350,7 @@ impl Compiler {
             let start_idx = self.instrs.next_idx();
 
             if let Some(expr) = case.expr() {
-                self.compile_expr(expr, dst);
+                self.compile_expr_dst(expr, *dst);
             }
 
             holes.push(self.instrs.add(Instr::Nop));
@@ -348,7 +372,7 @@ impl Compiler {
         }
     }
 
-    fn compile_expr_fn(&mut self, _expr: ExprFn, _dst: RegId) {
+    fn compile_expr_fn(&mut self, _expr: ExprFn, _dst: &mut RegId) {
         todo!()
     }
 
@@ -429,7 +453,7 @@ impl Compiler {
 pub fn compile(source: Arc<Source>, expr: Expr) {
     let mut compiler = Compiler::new(source);
     let reg = compiler.regs.alloc();
-    compiler.compile_expr(expr, reg);
+    compiler.compile_expr_dst(expr, reg);
 
     for diag in &compiler.diagnostics {
         eprintln!("{}", diag);
