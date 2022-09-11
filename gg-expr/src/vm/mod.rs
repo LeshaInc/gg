LeshaInc/gg

commit 44988b6eae8726aa56d5cfe58eb6f6effd7586ad

Add stack traces

gg-expr/src/vm/mod.rs
@@ -1,14 +1,18 @@
 mod consts;
+mod error;
 mod instr;
 mod ops;
 mod reg;
 
+use std::sync::Arc;
+
 pub use self::consts::{CompiledConsts, ConstId, Consts};
+pub use self::error::{Error, Result, StackFrame, StackTrace};
 pub use self::instr::{CompiledInstrs, Instr, InstrIdx, InstrOffset, Instrs, Opcode};
 pub use self::reg::{RegId, RegSeq, RegSeqIter};
-use crate::diagnostic::{Diagnostic, Severity};
-use crate::syntax::{BinOp, UnOp};
-use crate::{Error, Func, Result, Value};
+use crate::diagnostic::{Diagnostic, Severity, SourceComponent};
+use crate::syntax::{BinOp, TextRange, UnOp};
+use crate::{Func, FuncValue, Source, Value};
 
 #[derive(Debug, Default)]
 pub struct Vm {
@@ -87,16 +91,55 @@ impl Vm {
 }
 
 impl VmContext {
-    fn cur_func(&self) -> Result<&Func> {
-        self.stack
-            .get(self.frame.func)
-            .and_then(|v| v.as_func().ok())
-            .ok_or_else(|| self.func_invalid())
+    fn stack_trace(&self, range: Option<TextRange>) -> StackTrace {
+        let mut frames = Vec::with_capacity(self.frames.len() + 1);
+
+        frames.push(StackFrame {
+            range,
+            func: self.stack[self.frame.func].clone().try_into().unwrap(),
+        });
+
+        for frame in self.frames.iter().rev() {
+            let func = FuncValue::try_from(self.stack[frame.func].clone()).unwrap();
+
+            let range = func.debug_info.as_ref().and_then(|di| {
+                let prev_ip = &(frame.ip + InstrOffset(-1));
+                di.instruction_ranges
+                    .get(&prev_ip)
+                    .and_then(|v| v.get(0).copied())
+            });
+
+            frames.push(StackFrame { range, func });
+        }
+
+        StackTrace { frames }
     }
 
-    #[cold]
-    fn func_invalid(&self) -> Error {
-        Diagnostic::new(Severity::Error, "invalid function").into()
+    fn cur_ranges(&self) -> Option<Vec<TextRange>> {
+        if let Some(di) = &self.cur_func().debug_info {
+            let prev_ip = &(self.frame.ip + InstrOffset(-1));
+            return di.instruction_ranges.get(&prev_ip).cloned();
+        }
+        None
+    }
+
+    fn error(
+        &self,
+        range: Option<TextRange>,
+        message: impl Into<String>,
+        extra: impl FnOnce(&mut Diagnostic, Option<Arc<Source>>),
+    ) -> Error {
+        let debug_info = self.cur_func().debug_info.as_ref();
+        let source = debug_info.map(|v| v.source.clone());
+
+        let mut diagnostic = Diagnostic::new(Severity::Error, message.into());
+        extra(&mut diagnostic, source);
+
+        Error::new(diagnostic).with_stack_trace(self.stack_trace(range))
+    }
+
+    fn cur_func(&self) -> &Func {
+        self.stack[self.frame.func].as_func().unwrap()
     }
 
     fn reg_offset(&self, id: RegId) -> Result<usize> {
@@ -124,7 +167,7 @@ impl VmContext {
     }
 
     fn const_read(&self, id: ConstId) -> Result<&Value> {
-        let func = self.cur_func()?;
+        let func = self.cur_func();
         func.consts
             .0
             .get(usize::from(id.0))
@@ -137,7 +180,7 @@ impl VmContext {
     }
 
     fn fetch(&mut self) -> Result<Instr> {
-        let func = self.cur_func()?;
+        let func = self.cur_func();
         let instrs = &func.instrs.0;
         let instr = instrs
             .get(self.frame.ip.0 as usize)
@@ -241,7 +284,9 @@ impl VmContext {
         let dst_reg = instr.reg_c();
 
         let func_val = self.reg_read(func_reg)?;
-        let func = func_val.as_func().map_err(|_| self.func_invalid())?;
+        let func = func_val
+            .as_func()
+            .map_err(|_| self.error(None, "not a function", |_, _| ()))?;
 
         let old_base = self.frame.base;
         let new_base = self.stack.len();
@@ -278,7 +323,7 @@ impl VmContext {
     fn instr_ret(&mut self, instr: Instr) -> Result<()> {
         let val = self.reg_write(instr.reg_a(), Value::null())?;
 
-        let cur_func = self.cur_func()?;
+        let cur_func = self.cur_func();
         let num_slots = cur_func.slots;
         let dst = self.frame.dst;
 
@@ -377,7 +422,7 @@ impl VmContext {
     fn instr_bin_op(&mut self, instr: Instr, op: BinOp) -> Result<()> {
         let lhs = self.reg_read(instr.reg_a())?;
         let rhs = self.reg_read(instr.reg_b())?;
-        let res = ops::bin_op(op, lhs, rhs).ok_or_else(|| self.op_invalid())?;
+        let res = ops::bin_op(op, lhs, rhs).ok_or_else(|| self.error_bin_op(lhs, rhs, op))?;
         self.reg_write(instr.reg_c(), res)?;
         Ok(())
     }
@@ -385,13 +430,53 @@ impl VmContext {
     #[inline(always)]
     fn instr_un_op(&mut self, instr: Instr, op: UnOp) -> Result<()> {
         let arg = self.reg_read(instr.reg_a())?;
-        let res = ops::un_op(op, arg).ok_or_else(|| self.op_invalid())?;
+        let res = ops::un_op(op, arg).ok_or_else(|| self.error_un_op(arg, op))?;
         self.reg_write(instr.reg_b(), res)?;
         Ok(())
     }
 
     #[cold]
-    fn op_invalid(&self) -> Error {
-        Diagnostic::new(Severity::Error, "invalid op").into()
+    fn error_bin_op(&self, lhs: &Value, rhs: &Value, op: BinOp) -> Error {
+        let message = format!(
+            "operator `{}` cannot be applied to `{:?}` and `{:?}`",
+            op,
+            lhs.ty(),
+            rhs.ty()
+        );
+
+        let ranges = self.cur_ranges();
+        let main_range = ranges.as_ref().map(|v| v[0]);
+
+        self.error(main_range, message, |diag, source| {
+            if let (Some(source), Some(ranges)) = (source, ranges) {
+                diag.add_source(
+                    SourceComponent::new(source)
+                        .with_label(Severity::Error, ranges[1], format!("`{:?}`", lhs.ty()))
+                        .with_label(Severity::Error, ranges[2], format!("`{:?}`", rhs.ty())),
+                );
+            }
+        })
+    }
+
+    #[cold]
+    fn error_un_op(&self, arg: &Value, op: UnOp) -> Error {
+        let message = format!(
+            "unary operator `{}` cannot be applied to `{:?}`",
+            op,
+            arg.ty(),
+        );
+
+        let ranges = self.cur_ranges();
+        let main_range = ranges.as_ref().map(|v| v[0]);
+
+        self.error(main_range, message, |diag, source| {
+            if let (Some(source), Some(ranges)) = (source, ranges) {
+                diag.add_source(SourceComponent::new(source).with_label(
+                    Severity::Error,
+                    ranges[1],
+                    format!("`{:?}`", arg.ty()),
+                ));
+            }
+        })
     }
 }

gg-expr/src/compiler/mod.rs
@@ -20,6 +20,7 @@ pub struct Compiler {
     scopes: ScopeStack,
     diagnostics: Vec<Diagnostic>,
     debug_info: DebugInfo,
+    arity: u16,
 }
 
 impl Compiler {
@@ -31,6 +32,7 @@ impl Compiler {
             scopes: Default::default(),
             diagnostics: Default::default(),
             debug_info: DebugInfo::new(source),
+            arity: 0,
         }
     }
 
@@ -365,7 +367,11 @@ impl Compiler {
             let mut loc = tmp_reg;
 
             if let Some(expr) = binding.expr() {
-                self.compile_expr(expr, &mut loc);
+                if let Expr::Fn(v) = expr {
+                    self.compile_expr_fn_named(v, &mut loc, binding.ident());
+                } else {
+                    self.compile_expr(expr, &mut loc);
+                }
             }
 
             if loc != tmp_reg {
@@ -443,6 +449,7 @@ impl Compiler {
             self.scopes.set(arg, reg);
             num_args += 1;
         }
+        self.arity = num_args;
         self.regs.advance(num_args);
     }
 
@@ -454,7 +461,16 @@ impl Compiler {
     }
 
     fn compile_expr_fn(&mut self, expr: ExprFn, dst: &mut RegId) {
+        self.compile_expr_fn_named(expr, dst, None);
+    }
+
+    fn compile_expr_fn_named(&mut self, expr: ExprFn, dst: &mut RegId, name: Option<Ident>) {
         let mut compiler = Compiler::new(self.debug_info.source.clone());
+        compiler.debug_info.name = Some(
+            name.map(|v| v.name().into())
+                .unwrap_or_else(|| "<anon>".into()),
+        );
+        compiler.debug_info.range = expr.range();
 
         if let Some(body) = expr.expr() {
             compiler.compile_fn(expr.args(), body);
@@ -547,6 +563,7 @@ impl Compiler {
     fn finish(self) -> CompileResult {
         CompileResult {
             func: Func {
+                arity: self.arity,
                 slots: self.regs.slots(),
                 instrs: self.instrs.compile(),
                 consts: self.consts.compile(),
@@ -559,6 +576,8 @@ impl Compiler {
 
 pub fn compile(source: Arc<Source>, expr: Expr) -> CompileResult {
     let mut compiler = Compiler::new(source);
+    compiler.debug_info.name = Some("<main>".into());
+    compiler.debug_info.range = expr.range();
     compiler.compile_fn(iter::empty(), expr);
     compiler.finish()
 }

gg-expr/src/value/mod.rs
@@ -4,6 +4,7 @@ use std::fmt::{self, Debug};
 use std::hash::{Hash, Hasher};
 use std::hint::unreachable_unchecked;
 use std::mem::ManuallyDrop;
+use std::ops::Deref;
 use std::sync::atomic::AtomicUsize;
 use std::sync::atomic::Ordering::{Acquire, Release};
 
@@ -543,6 +544,44 @@ impl<'a> TryFrom<&'a Value> for &'a Map {
     type Error = FromValueError;
     fn try_from(v: &'a Value) -> Result<&'a Map, FromValueError> {
         v.as_map()
+    }
+}
+
+#[derive(Clone, Eq, PartialEq, Hash)]
+pub struct FuncValue(Value);
+
+impl Deref for FuncValue {
+    type Target = Func;
+
+    fn deref(&self) -> &Func {
+        unsafe { self.0.as_func().unwrap_unchecked() }
+    }
+}
+
+impl From<FuncValue> for Value {
+    fn from(v: FuncValue) -> Self {
+        v.0
+    }
+}
+
+impl TryFrom<Value> for FuncValue {
+    type Error = FromValueError;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        if value.is_func() {
+            Ok(FuncValue(value))
+        } else {
+            Err(FromValueError {
+                expected: Type::Func,
+                got: value.ty(),
+            })
+        }
+    }
+}
+
+impl Debug for FuncValue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
     }
 }
 

gg-expr/src/diagnostic.rs
@@ -55,13 +55,21 @@ impl Diagnostic {
         }
     }
 
-    pub fn with_help(mut self, help: String) -> Diagnostic {
-        self.components.push(Component::Help(help));
+    pub fn add_help(&mut self, help: impl Into<String>) {
+        self.components.push(Component::Help(help.into()));
+    }
+
+    pub fn with_help(mut self, help: impl Into<String>) -> Diagnostic {
+        self.add_help(help);
         self
     }
 
-    pub fn with_source(mut self, source: SourceComponent) -> Diagnostic {
+    pub fn add_source(&mut self, source: SourceComponent) {
         self.components.push(Component::Source(source));
+    }
+
+    pub fn with_source(mut self, source: SourceComponent) -> Diagnostic {
+        self.add_source(source);
         self
     }
 }
@@ -453,6 +461,10 @@ impl HlLine {
 
         while label_pos < pos + len && self.labels.iter().any(|&(pos, ..)| pos == label_pos) {
             label_pos += 1;
+        }
+
+        if label.message.trim().is_empty() {
+            return;
         }
 
         cells[label_pos - pos] = ('┯', color);

gg-expr/src/error.rs
@@ -1,45 +1,0 @@
-use std::fmt::{self, Debug, Display};
-
-use crate::diagnostic::Diagnostic;
-
-pub type Result<T> = std::result::Result<T, Error>;
-
-pub struct Error {
-    inner: Box<ErrorInner>,
-}
-
-struct ErrorInner {
-    diagnostic: Diagnostic,
-}
-
-impl Error {
-    pub fn new(diagnostic: Diagnostic) -> Error {
-        Error {
-            inner: Box::new(ErrorInner { diagnostic }),
-        }
-    }
-
-    pub fn diagnostic(&self) -> &Diagnostic {
-        &self.inner.diagnostic
-    }
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.diagnostic())
-    }
-}
-
-impl Debug for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self)
-    }
-}
-
-impl From<Diagnostic> for Error {
-    fn from(v: Diagnostic) -> Self {
-        Self::new(v)
-    }
-}
-
-impl std::error::Error for Error {}

gg-expr/src/lib.rs
@@ -1,15 +1,13 @@
 pub mod compiler;
 pub mod diagnostic;
-mod error;
 mod source;
 pub mod syntax;
 mod value;
 pub mod vm;
 
 pub use self::compiler::{compile, Compiler};
-pub use self::error::{Error, Result};
 pub use self::source::{LineColPos, LineColRange, Source, SourceText};
-pub use self::value::{DebugInfo, Func, Type, Value};
+pub use self::value::{DebugInfo, Func, FuncValue, Type, Value};
 pub use self::vm::Vm;
 use crate::diagnostic::Diagnostic;
 

gg-expr/src/main.rs
@@ -19,12 +19,18 @@ fn main() {
 
     println!();
     println!("{:?}", func);
+    println!();
 
     let mut vm = Vm::new();
     let t = std::time::Instant::now();
-    let result = vm.eval(&func, &[]);
+
+    match vm.eval(&func, &[]) {
+        Ok(v) => println!("{:?}", v),
+        Err(e) => {
+            eprintln!("{}", e);
+        }
+    }
+
     let elapsed = t.elapsed();
-    println!();
-    println!("{:?}", result);
     println!("elapsed {:?}", elapsed);
 }

gg-expr/src/value/func.rs
@@ -11,6 +11,7 @@ use crate::Source;
 
 #[derive(Clone)]
 pub struct Func {
+    pub arity: u16,
     pub slots: u16,
     pub instrs: CompiledInstrs,
     pub consts: CompiledConsts,
@@ -20,10 +21,12 @@ pub struct Func {
 impl Debug for Func {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         if let Some(name) = self.debug_info.as_ref().and_then(|di| di.name.as_ref()) {
-            write!(f, "fn {}(...):", name)?;
+            write!(f, "fn {}", name)?;
         } else {
-            write!(f, "fn(...):")?;
+            write!(f, "fn")?;
         }
+
+        write!(f, "({} args) {{", self.arity)?;
 
         if let Some(di) = &self.debug_info {
             let range = di.source.text.range_to_line_col(di.range);
@@ -32,7 +35,8 @@ impl Debug for Func {
 
         writeln!(f)?;
 
-        let mut f = indented(f);
+        let mut orig_f = f;
+        let mut f = indented(&mut orig_f);
 
         for (i, val) in self.consts.0.iter().enumerate() {
             writeln!(f, "{}: {:?}", i, val)?;
@@ -64,6 +68,9 @@ impl Debug for Func {
                 write!(f, "{}", range)?;
             }
         }
+
+        writeln!(f)?;
+        write!(orig_f, "}}")?;
 
         Ok(())
     }

commit 597ee6719925a39d15d4c08044f7734518a9225e

Fix performance regression caused in 44988b6eae

gg-expr/src/vm/mod.rs
@@ -116,7 +116,7 @@ impl VmContext {
     }
 
     fn cur_ranges(&self) -> Option<Vec<TextRange>> {
-        if let Some(di) = &self.cur_func().debug_info {
+        if let Some(di) = &self.cur_func().ok()?.debug_info {
             let prev_ip = &(self.frame.ip + InstrOffset(-1));
             return di.instruction_ranges.get(&prev_ip).cloned();
         }
@@ -129,7 +129,7 @@ impl VmContext {
         message: impl Into<String>,
         extra: impl FnOnce(&mut Diagnostic, Option<Arc<Source>>),
     ) -> Error {
-        let debug_info = self.cur_func().debug_info.as_ref();
+        let debug_info = self.cur_func().ok().and_then(|f| f.debug_info.as_ref());
         let source = debug_info.map(|v| v.source.clone());
 
         let mut diagnostic = Diagnostic::new(Severity::Error, message.into());
@@ -138,22 +138,35 @@ impl VmContext {
         Error::new(diagnostic).with_stack_trace(self.stack_trace(range))
     }
 
-    fn cur_func(&self) -> &Func {
-        self.stack[self.frame.func].as_func().unwrap()
+    #[inline(never)]
+    fn error_simple(&self, message: &str) -> Error {
+        self.error(None, message, |_, _| ())
+    }
+
+    fn cur_func(&self) -> Result<&Func> {
+        self.stack
+            .get(self.frame.func)
+            .and_then(|v| v.as_func().ok())
+            .ok_or_else(|| self.error_bad_fn())
+    }
+
+    #[inline(never)]
+    fn error_bad_fn(&self) -> Error {
+        self.error_simple("invalid function")
     }
 
     fn reg_offset(&self, id: RegId) -> Result<usize> {
         let idx = self.frame.base + usize::from(id.0);
         if idx >= self.stack.len() {
-            Err(self.reg_invalid())
+            Err(self.error_bad_reg())
         } else {
             Ok(idx)
         }
     }
 
-    #[cold]
-    fn reg_invalid(&self) -> Error {
-        Diagnostic::new(Severity::Error, "invalid register").into()
+    #[inline(never)]
+    fn error_bad_reg(&self) -> Error {
+        self.error_simple("invalid register")
     }
 
     fn reg_read(&self, id: RegId) -> Result<&Value> {
@@ -167,32 +180,32 @@ impl VmContext {
     }
 
     fn const_read(&self, id: ConstId) -> Result<&Value> {
-        let func = self.cur_func();
+        let func = self.cur_func()?;
         func.consts
             .0
             .get(usize::from(id.0))
-            .ok_or_else(|| self.const_invalid())
+            .ok_or_else(|| self.error_bad_const())
     }
 
-    #[cold]
-    fn const_invalid(&self) -> Error {
-        Diagnostic::new(Severity::Error, "invalid register").into()
+    #[inline(never)]
+    fn error_bad_const(&self) -> Error {
+        self.error_simple("invalid constant")
     }
 
     fn fetch(&mut self) -> Result<Instr> {
-        let func = self.cur_func();
+        let func = self.cur_func()?;
         let instrs = &func.instrs.0;
         let instr = instrs
             .get(self.frame.ip.0 as usize)
             .copied()
-            .ok_or_else(|| self.code_overrun())?;
+            .ok_or_else(|| self.error_code_overrun())?;
         self.frame.ip += InstrOffset(1);
         Ok(instr)
     }
 
-    #[cold]
-    fn code_overrun(&self) -> Error {
-        Diagnostic::new(Severity::Error, "code overrun").into()
+    #[inline(never)]
+    fn error_code_overrun(&self) -> Error {
+        self.error_simple("code overrun")
     }
 
     fn dispatch(&mut self, instr: Instr) -> Result<()> {
@@ -235,7 +248,12 @@ impl VmContext {
     }
 
     fn instr_panic(&mut self, _instr: Instr) -> Result<()> {
-        Err(Diagnostic::new(Severity::Error, "vm panicked").into())
+        Err(self.error_panic())
+    }
+
+    #[inline(never)]
+    fn error_panic(&self) -> Error {
+        self.error_simple("panic")
     }
 
     fn instr_copy(&mut self, instr: Instr) -> Result<()> {
@@ -284,9 +302,7 @@ impl VmContext {
         let dst_reg = instr.reg_c();
 
         let func_val = self.reg_read(func_reg)?;
-        let func = func_val
-            .as_func()
-            .map_err(|_| self.error(None, "not a function", |_, _| ()))?;
+        let func = func_val.as_func().map_err(|_| self.error_bad_fn())?;
 
         let old_base = self.frame.base;
         let new_base = self.stack.len();
@@ -323,7 +339,7 @@ impl VmContext {
     fn instr_ret(&mut self, instr: Instr) -> Result<()> {
         let val = self.reg_write(instr.reg_a(), Value::null())?;
 
-        let cur_func = self.cur_func();
+        let cur_func = self.cur_func()?;
         let num_slots = cur_func.slots;
         let dst = self.frame.dst;
 
@@ -422,7 +438,7 @@ impl VmContext {
     fn instr_bin_op(&mut self, instr: Instr, op: BinOp) -> Result<()> {
         let lhs = self.reg_read(instr.reg_a())?;
         let rhs = self.reg_read(instr.reg_b())?;
-        let res = ops::bin_op(op, lhs, rhs).ok_or_else(|| self.error_bin_op(lhs, rhs, op))?;
+        let res = ops::bin_op(op, lhs, rhs).ok_or_else(|| self.error_bin_op(instr, op))?;
         self.reg_write(instr.reg_c(), res)?;
         Ok(())
     }
@@ -430,13 +446,16 @@ impl VmContext {
     #[inline(always)]
     fn instr_un_op(&mut self, instr: Instr, op: UnOp) -> Result<()> {
         let arg = self.reg_read(instr.reg_a())?;
-        let res = ops::un_op(op, arg).ok_or_else(|| self.error_un_op(arg, op))?;
+        let res = ops::un_op(op, arg).ok_or_else(|| self.error_un_op(instr, op))?;
         self.reg_write(instr.reg_b(), res)?;
         Ok(())
     }
 
-    #[cold]
-    fn error_bin_op(&self, lhs: &Value, rhs: &Value, op: BinOp) -> Error {
+    #[inline(never)]
+    fn error_bin_op(&self, instr: Instr, op: BinOp) -> Error {
+        let lhs = self.reg_read(instr.reg_a()).unwrap();
+        let rhs = self.reg_read(instr.reg_b()).unwrap();
+
         let message = format!(
             "operator `{}` cannot be applied to `{:?}` and `{:?}`",
             op,
@@ -458,8 +477,10 @@ impl VmContext {
         })
     }
 
-    #[cold]
-    fn error_un_op(&self, arg: &Value, op: UnOp) -> Error {
+    #[inline(never)]
+    fn error_un_op(&self, instr: Instr, op: UnOp) -> Error {
+        let arg = self.reg_read(instr.reg_a()).unwrap();
+
         let message = format!(
             "unary operator `{}` cannot be applied to `{:?}`",
             op,